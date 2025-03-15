623d477fd931663095ac4f19f4de29febdbc26ba
Raul Gheba <94930427+RaulGheba@users.noreply.github.com>
2025-03-15 20:47:56 +0200

    adding tailwind

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import {Outlet} from "react-router-dom";
+import React from "react";
+import { Outlet } from "react-router-dom";
 import Header from "./Header.jsx";
 import Sidebar from "./Sidebar.jsx";
 
 function AppLayout(props) {
-    return (
-        <div>
-        <Header/>
-            <Sidebar/>
-        <Outlet/>
-        </div>
-    );
+  return (
+    <div className=" mt-0.5 mr-6 ml-6">
+      <Header />
+      <Sidebar />
+      <Outlet />
+    </div>
+  );
 }
 
 export default AppLayout;
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,9 +1,47 @@
-import React from 'react';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 function Header(props) {
-    return (
-        <div>HEADER</div>
-    );
+  return (
+    <div className="flex justify-between px-12 font-[poppins] bg-gray-500 text-white py-2">
+      <NavLink
+        className="hover:text-red-400 transition-colors duration-300"
+        to="/"
+      >
+        Home
+      </NavLink>
+      <NavLink
+        className="hover:text-red-400 transition-colors duration-300"
+        to="add-workout"
+      >
+        Workouts
+      </NavLink>
+      <NavLink
+        className="hover:text-red-400 transition-colors duration-300"
+        to="account"
+      >
+        Account
+      </NavLink>
+      <NavLink
+        className="hover:text-red-400 transition-colors duration-300"
+        to="library"
+      >
+        Library
+      </NavLink>
+      <NavLink
+        className="hover:text-red-400 transition-colors duration-300"
+        to="store"
+      >
+        Store
+      </NavLink>
+      <NavLink
+        className="hover:text-red-400 transition-colors duration-300"
+        to="support"
+      >
+        Support
+      </NavLink>
+    </div>
+  );
 }
 
 export default Header;
